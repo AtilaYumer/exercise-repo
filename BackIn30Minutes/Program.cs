f1bb2c29c4cb03f94bbf1eafbaf3a32e602f1cf0
AtillaOmer <atillayumer@gmail.com>
2017-05-31 19:52:18 +0300

    Add files via upload
    Back in 30 minutes exercise

---
+++ BackIn30Minutes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 30;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes = minutes - 60;
+            }
+            if (hours > 23)
+            {
+                hours = hours - 24;
+            }
+            if (minutes > 9)
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+        }
+    }
+}
